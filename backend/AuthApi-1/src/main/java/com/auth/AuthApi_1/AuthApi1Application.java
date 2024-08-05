7f0ca4d99bd037ad79e07cc0bcc41351ad84e1a1
saikirun7 <saikirunrayudu@gmail.com>
2024-08-06 01:49:49 +0530

    AuthApi-1 commit

---
+++ backend/AuthApi-1/src/main/java/com/auth/AuthApi_1/AuthApi1Application.java
@@ -0,0 +1,13 @@
+package com.auth.AuthApi_1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthApi1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthApi1Application.class, args);
+	}
+
+}
---
+++ backend/AuthApi-1/src/test/java/com/auth/AuthApi_1/AuthApi1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.auth.AuthApi_1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthApi1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
